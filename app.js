cb72acda419c7afbf022a3798493b65ede40266c
Vinicius Ventura <42718816+ventura-v@users.noreply.github.com>
2025-02-02 23:06:03 +0000

    chore: add absolute path config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = 3000
 
 const path = '/api/v1/status'
 
@@ -21,3 +21,7 @@ app.get(path, (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+
+// app.listen(`${port}/${path}`, () => {
+//     console.log(`Server is running on port ${port}/${path}`)
+// })
