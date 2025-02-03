596b25c430c57c055e986796607c2df345c7f595
Vinicius Ventura <42718816+ventura-v@users.noreply.github.com>
2025-02-03 00:59:16 +0000

    feat: finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,34 @@
-function status(req, res) {
-    res.status(200).json({ key: "API running" })
+const database = require("../../../../infra/database")
+
+async function status(req, res) {
+    const updatedAt = new Date().toISOString()
+
+    // Query para pegar a versão do banco de dados
+    const databaseVersionResult = await database.query("SHOW server_version;")
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version
+
+    // Query para pegar a quantidade máxima de conexões
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+
+    // Query para pegar a quantidade de conexões ativas
+    const databaseName = process.env.POSTGRES_DB
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName],
+    })
+    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
+
+    res.status(200).json({
+        updated_at: updatedAt,
+        dependecies: {
+            database: {
+                version: databaseVersionValue,
+                max_connections: +databaseMaxConnectionsValue,
+                opened_connections: databaseOpenedConnectionsValue,
+            },
+        },
+    })
 }
 
-export default status
+module.exports = status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,5 +3,17 @@ test("GET to /api/v1/status should return 200", async () => {
     expect(response.status).toBe(200)
 
     const responseBody = await response.json()
-    console.log(responseBody)
+
+    //verificando se o valor passado para a data é válido
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+
+    //verificando a versão do banco de dados
+    expect(responseBody.dependecies.database.version).toEqual("17.2")
+
+    //verificando a quantidade máxima de conexões
+    expect(responseBody.dependecies.database.max_connections).toEqual(100)
+
+    //verificando se a quantidade de conexões ativas é igual a 1 (se for maior, provavelmente é algum vazamento)  
+    expect(responseBody.dependecies.database.opened_connections).toEqual(1)
 })
