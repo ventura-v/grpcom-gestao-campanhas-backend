3b89e8382b3653941b3389f4f20f56bf6485e919
Vinicius Ventura <42718816+ventura-v@users.noreply.github.com>
2025-02-03 01:00:40 +0000

    refact: backend conection

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require('express')
+const status = require('./pages/api/v1/status/index.js')
 
 const app = express()
-
 const port = 3000
-
 const path = '/api/v1/status'
-
-// const status = require('./pages/api/v1/status')
-
-// app.use(path, status)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the API');
   });
 
-app.get(path, (req, res) => {
-    res.status(200).json({ key: "API running" })
-})
+app.get(path, status)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
-
-// app.listen(`${port}/${path}`, () => {
-//     console.log(`Server is running on port ${port}/${path}`)
-// })
